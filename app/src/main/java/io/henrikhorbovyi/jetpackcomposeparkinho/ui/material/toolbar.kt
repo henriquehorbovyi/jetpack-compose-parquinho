5df01d01bceca74ebb751465778b9b9153259db8
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2020-06-04 20:04:44 -0300

    improved toolbar sample

--- app/src/main/java/io/henrikhorbovyi/jetpackcomposeparkinho/ui/material/toolbar.kt
+++ app/src/main/java/io/henrikhorbovyi/jetpackcomposeparkinho/ui/material/toolbar.kt
@@ -1,18 +1,17 @@
 package io.henrikhorbovyi.jetpackcomposeparkinho.ui.material
 
 import androidx.compose.Composable
-import androidx.ui.core.Modifier
-import androidx.ui.foundation.ContentGravity
+import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Text
 import androidx.ui.graphics.Color
-import androidx.ui.layout.Arrangement
-import androidx.ui.layout.Column
-import androidx.ui.layout.fillMaxSize
+import androidx.ui.material.IconButton
 import androidx.ui.material.Scaffold
 import androidx.ui.material.TopAppBar
+import androidx.ui.material.icons.Icons
+import androidx.ui.material.icons.filled.Menu
+import androidx.ui.material.icons.filled.Search
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
-import androidx.ui.unit.sp
 import io.henrikhorbovyi.jetpackcomposeparkinho.ui.SampleActivity
 
 class ToolbarActivity : SampleActivity() {
@@ -36,16 +35,13 @@ fun SimpleToolbarSample() {
             TopAppBar(
                 backgroundColor = Color.Red,
                 elevation = 8.dp,
-                content = {
-                    /*
-                    * Você pode personalizar como quiser dentro aqui (escopo do "content")
-                    * */
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalGravity = ContentGravity.CenterHorizontally
-                    ) {
-                        Text("Toolbar", color = Color.White, fontSize = 22.sp)
+                title = { Text("Atividades", color = Color.White) },
+                navigationIcon = { IconButton(onClick = {}) {
+                    Icon(asset = Icons.Default.Menu, tint = Color.White) }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(asset = Icons.Default.Search, tint = Color.White)
                     }
                 }
             )
