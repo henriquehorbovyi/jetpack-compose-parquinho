05a8d19ae31a335c8b717f36ab867e1fdced28da
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2020-06-22 12:45:09 -0300

    strings and drawable samples

---
+++ app/src/main/java/io/henrikhorbovyi/jetpackcomposeparkinho/ui/resources/strings.kt
@@ -0,0 +1,32 @@
+package io.henrikhorbovyi.jetpackcomposeparkinho.ui.resources
+
+import androidx.compose.Composable
+import androidx.ui.foundation.Text
+import androidx.ui.graphics.Color
+import androidx.ui.res.stringResource
+import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.sp
+import io.henrikhorbovyi.jetpackcomposeparkinho.R
+import io.henrikhorbovyi.jetpackcomposeparkinho.ui.Center
+import io.henrikhorbovyi.jetpackcomposeparkinho.ui.SampleActivity
+
+class StringsActivity : SampleActivity() {
+
+    @Composable
+    override fun Sample() {
+        StringsSample()
+    }
+
+    @Preview("stringsPreview", showDecoration = true)
+    @Composable
+    override fun preview() {
+        StringsSample()
+    }
+}
+
+@Composable
+fun StringsSample() {
+    Center {
+        Text(stringResource(id = R.string.app_name), color = Color.Black, fontSize = 22.sp)
+    }
+}
